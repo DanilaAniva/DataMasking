061ea8f16c3379a5dd3e2cddf6bf6eb5d2bdd85e
DanilaAniva <danila@osipova.net>
2023-05-14 14:26:27 +0300

    DP with Laplace

--- scripts/Differencial Privacy.py
+++ scripts/Differencial Privacy.py
@@ -44,9 +44,10 @@ np.random.seed(42)
 # plt.show()
 
 
-#Предобработка данных. Вычисление чувствительности.
+
 
 def filter_bounds(value, lower_bound, upper_bound):
+    '''#Предобработка данных. Вычисление чувствительности.'''
     if value < lower_bound:
         return lower_bound
     elif value > upper_bound:
@@ -54,43 +55,55 @@ def filter_bounds(value, lower_bound, upper_bound):
     return value
 
 def filter_data(df, column, min_value, max_value):
+    '''#Обработка по датафрейму'''
     df[column] = df[column].apply(lambda x: filter_bounds(x, min_value, max_value))
 
-#Алгоритм Лапласа
+
 def laplace_dp_mechanism(value, epsilon, sensitivity=1):
+    '''#Алгоритм Лапласа'''
     orig_value = value
     value =  np.random.laplace(value, sensitivity/epsilon)
     # print("Noise: {}".format(value - orig_value))
     return value
-def mask_df_age_primitive_DP(df,column):
+
+def mask_df_age_DP(df, column):
+    '''#Маскирование датафрейма с алгоритмом Лапласа. Вычисление чувствительности как 1/кол-во записей'''
     df[column] = df[column].apply(lambda x: filter_bounds(x, 18, 120))
     sensitivity = 1/len(df[column])
     df[column] = df[column].apply(lambda x: int(laplace_dp_mechanism(x, 0.5, sensitivity= sensitivity )))
+def mask_df_salary_DP(df,column):
+    df[column] = df[column].apply(lambda x: filter_bounds(x,0, 1000000))
+    sensitivity = 1/len(df[column])
+    df[column] = df[column].apply(lambda x: int(laplace_dp_mechanism(x, 0.5, sensitivity=sensitivity)))
 
 def main():
+    '''Сделаем совокупность показателей возраста и заработной платы'''
     mid_level_age = 45
     mid_level_salary = 50000
 
-    age_scale = 10  # scale represents one standard deviation
+    age_scale = 10
     salary_scale = 10000
 
     salaries = [round(np.random.normal(mid_level_salary, salary_scale)) for _ in range(100)]
     ages = [round(np.random.normal(mid_level_age, age_scale)) for _ in range(100)]
+    '''Фильтруем данные'''
     bounded_ages = [filter_bounds(age, 20, 70) for age in ages]
 
     epsilon_for_sum = 0.5
     epsilon_for_count = 0.5
 
+    '''Механизм лапласа для суммы и count'''
     summed_ages = laplace_dp_mechanism(np.sum(bounded_ages), epsilon_for_sum, sensitivity=50)
     count_ages = laplace_dp_mechanism(len(bounded_ages), epsilon_for_count, sensitivity=1)
     print("\n"), print(bounded_ages[:10])
     print('Mean: '), print(np.mean(bounded_ages))
 
+    '''Механизм Лапласа для совокупности показателей возраста'''
     bounded_ages = laplace_dp_mechanism(bounded_ages, 0.5, sensitivity=5)
+    plt.hist(bounded_ages)
+    plt.show()
     print(bounded_ages)
     print('\n'), print(bounded_ages[:10])
     print('Mean: '), print(np.mean(bounded_ages))
-    average_age = summed_ages / count_ages
-    #
     # Test = pd.DataFrame({'Age':[33, 44, 22, 11]})
 main()
